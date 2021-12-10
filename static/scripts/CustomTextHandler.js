570ee95dec049856f402f9bb495a41ea6c0b34f4
Christa Bridges <christa@pebblepad.co.uk>
2021-12-10 13:36:31 +0000

    message Changed positioning of the text and tree

--- static/scripts/CustomTextHandler.js
+++ static/scripts/CustomTextHandler.js
@@ -13,27 +13,5 @@ class CustomTextHandler {
     updateText(customText) {
         const aText = document.querySelector("#customOutput");
         aText.setAttribute("value", customText);
-        fetch("./static/fonts/Gwendolyn-Bold-msdf.json")
-        .then(response => response.json())
-        .then(jsonResponse => {
-            let length = 0;
-            for (const character of customText) {
-                for (let charInfo of jsonResponse.chars) {
-
-                    if (charInfo.char == character) {
-                        console.log(charInfo)
-                        length += charInfo.width;
-                    }
-                }
-            }
-
-            console.log(length);
-
-            const position = (0.56521739130434782608695652173913 * length);
-            // console.log((0.56521739130434782608695652173913 * length))
-            // console.log(position)
-
-            aText.setAttribute("position", `${position} -65 -110`)
-        })
     }
 }
